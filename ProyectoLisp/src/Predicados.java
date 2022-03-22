e092cb812636d25f2e0ae4481f2925b6ee314e72
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2022-03-22 15:21:36 -0600

    Creacion de la clase Predicados

---
+++ ProyectoLisp/src/Predicados.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Predicados {
+    public ArrayList<String> list(){
+
+        return null;
+    }
+
+    public ArrayList<String> equal(){
+
+        return null;
+    }
+
+    public ArrayList<String> menor(){
+
+        return null;
+    }
+
+    public ArrayList<String> mayor(){
+
+        return null;
+    }
+}
